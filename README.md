795a230e493bf98d0cde815dd6d222577a5ff70e
Zoldur <zoldur99@gmail.com>
2018-03-10 13:52:10 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Olympic Coin
+Shell script to install an [Olympic Masternode](https://olympiccoin.cash/) on a Linux server running **Ubuntu 14.04**. Use it on your own risk.
+***
+
+## Installation
+```
+wget -q https://raw.githubusercontent.com/zoldur/OlympicCoin/master/olympic_install.sh  
+bash olympic_install.sh
+```
+***
+
+## Desktop wallet setup  
+
+After the MN is up and running, you need to configure the desktop wallet accordingly. Here are the steps:  
+1. Open the Olympicd Wallet.  
+2. Go to RECEIVE and create a New Address: **MN1**  
+3. Send **1500** **OLYMP** to **MN1**.  
+4. Wait for 16 confirmations.  
+5. Go to **Help -> "Debug Window - Console"**  
+6. Type the following command: **masternode outputs**  
+7. Go to **Masternodes** tab  
+8. Click **Create** and fill the details:  
+* Alias: **MN1**  
+* Address: **VPS_IP:PORT**  
+* Privkey: **Masternode Private Key**  
+* TxHash: **First value from Step 6**  
+* Output index:  **Second value from Step 6**  
+* Reward address: leave blank  
+* Reward %: leave blank  
+9. Click **OK** to add the masternode  
+10. Click **Start All**  
+***
+
+## Usage:
+```
+Olympicd masternode status  
+Olympicd getinfo
+```
+Also, if you want to start/stop **Olympic**, run one of the following commands as **root**:
+
+```
+/etc/init.d/Olympic start #To start Olympic service  
+/etc/init.d/Olympic stop #To stop Olympic service
+```  
+***
+
+## Donations
+
+Any donation is highly appreciated  
+
+
+**OLYMP**: QgJKe65M49ZEnPAhyDC83hXzLALpGHJcHJ
+**BTC**: 3MQLEcHXVvxpmwbB811qiC1c6g21ZKa7Jh  
+**ETH**: 0x39d10fe57611c564abc255ffd7e984dc97e9bd6d  
+**LTC**: LNZpK4rCd1JVSB3rGKTAnTkudV9So9zexB
---
+++ olympic_install.sh
@@ -0,0 +1,288 @@
+#!/bin/bash
+
+TMP_FOLDER=$(mktemp -d)
+CONFIG_FILE='Olympic.conf'
+CONFIGFOLDER='/root/.Olympic'
+COIN_DAEMON='/usr/local/bin/Olympicd'
+COIN_CLI='/usr/local/bin/Olympicd'
+COIN_REPO='https://github.com/OlympicCoin/olympic'
+COIN_NAME='Olympic'
+COIN_PORT=26667
+RPC_PORT=26666
+INSTALLED=0
+
+NODEIP=$(curl -s4 icanhazip.com)
+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+function delete_wallet() {
+ if [ -s "$CONFIGFOLDER/$CONFIG_FILE" ]; then
+   /etc/init.d/$COIN_NAME stop
+   pkill $COIN_DAEMON >/dev/null 2>&1
+   sleep 5
+   rsync -a $CONFIGFOLDER/ $CONFIGFOLDER.bkp/
+   rm -r $CONFIGFOLDER/{smsgDB,txleveldb,smsg.ini,peers.dat,mncache.dat,blk0001.dat}
+   INSTALLED=1
+ fi
+}
+
+function compile_node() {
+  echo -e "Prepare to compile $COIN_NAME"
+  git clone  $COIN_REPO $TMP_FOLDER
+  cd $TMP_FOLDER/src
+  make -f makefile.unix
+  compile_error
+  strip Olympicd
+  cp Olympicd /usr/local/bin
+  cd -
+  #rm -rf $TMP_FOLDER >/dev/null 2>&1
+  clear
+}
+
+function configure_startup() {
+  cat << EOF > /etc/init.d/$COIN_NAME
+#! /bin/bash
+### BEGIN INIT INFO
+# Provides: $COIN_NAME
+# Required-Start: $remote_fs $syslog
+# Required-Stop: $remote_fs $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: $COIN_NAME
+# Description: This file starts and stops $COIN_NAME MN server
+#
+### END INIT INFO
+
+case "\$1" in
+ start)
+   $COIN_DAEMON -daemon
+   sleep 5
+   ;;
+ stop)
+   $COIN_NAME stop
+   ;;
+ restart)
+   $COIN_DAEMON stop
+   sleep 10
+   $COIN_DAEMON -daemon
+   ;;
+ *)
+   echo "Usage: $COIN_NAME {start|stop|restart}" >&2
+   exit 3
+   ;;
+esac
+EOF
+chmod +x /etc/init.d/$COIN_NAME >/dev/null 2>&1
+update-rc.d $COIN_NAME defaults >/dev/null 2>&1
+/etc/init.d/$COIN_NAME start
+if [ "$?" -gt "0" ]; then
+ sleep 5
+ /etc/init.d/$COIN_NAME start
+ if [ "$?" -gt "0" ]; then
+  echo -e "${RED}Could not start $COIN_NAME. Please start it by running:${GREEN}/etc/init.d/COIN_NAME start${NC}"
+ fi
+fi
+}
+
+
+function create_config() {
+  mkdir $CONFIGFOLDER >/dev/null 2>&1
+  RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w12 | head -n1)
+  RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w24 | head -n1)
+  cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
+rpcuser=$RPCUSER
+rpcpassword=$RPCPASSWORD
+rpcallowip=127.0.0.1
+rpcport=$RPC_PORT
+listen=1
+server=1
+daemon=1
+port=$COIN_PORT
+EOF
+}
+
+function create_key() {
+  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+  read -e COINKEY
+  if [[ -z "$COINKEY" ]]; then
+  $COIN_DAEMON -daemon
+  sleep 30
+  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
+   echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
+   exit 1
+  fi
+  COINKEY=$($COIN_CLI masternode genkey)
+  if [ "$?" -gt "0" ];
+    then
+    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
+    sleep 30
+    COINKEY=$($COIN_CLI masternode genkey)
+  fi
+  $COIN_CLI stop
+fi
+clear
+}
+
+function update_config() {
+  sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
+  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
+logintimestamps=1
+maxconnections=256
+masternode=1
+#bind=$NODEIP
+masternodeaddr=$NODEIP:$COIN_PORT
+masternodeprivkey=$COINKEY
+EOF
+}
+
+
+function enable_firewall() {
+  echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
+  ufw allow $COIN_PORT/tcp >/dev/null 2>&1
+  ufw allow ssh comment "SSH" >/dev/null 2>&1
+  ufw limit ssh/tcp >/dev/null 2>&1
+  ufw default allow outgoing >/dev/null 2>&1
+  echo "y" | ufw enable >/dev/null 2>&1
+}
+
+
+
+function get_ip() {
+  declare -a NODE_IPS
+  for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
+  do
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+  done
+
+  if [ ${#NODE_IPS[@]} -gt 1 ]
+    then
+      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
+      INDEX=0
+      for ip in "${NODE_IPS[@]}"
+      do
+        echo ${INDEX} $ip
+        let INDEX=${INDEX}+1
+      done
+      read -e choose_ip
+      NODEIP=${NODE_IPS[$choose_ip]}
+  else
+    NODEIP=${NODE_IPS[0]}
+  fi
+}
+
+
+function compile_error() {
+if [ "$?" -gt "0" ];
+ then
+  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
+  exit 1
+fi
+}
+
+
+function checks() {
+if [[ $(lsb_release -d) != *14.04* ]]; then
+  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+  exit 1
+fi
+
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}$0 must be run as root.${NC}"
+   exit 1
+fi
+
+#if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMOM" ] ; then
+#  echo -e "${RED}$COIN_NAME is already installed.${NC}"
+#  exit 1
+#fi
+}
+
+function prepare_system() {
+echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
+apt-get update >/dev/null 2>&1
+DEBIAN_FRONTEND=noninteractive apt-get update >/dev/null 2>&1
+DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
+apt-get install -y software-properties-common >/dev/null 2>&1
+echo -e "${GREEN}Adding bitcoin PPA repository"
+apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
+echo -e "Installing required packages, it may take some time to finish.${NC}"
+apt-get update >/dev/null 2>&1
+apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
+build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
+libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
+libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  >/dev/null 2>&1
+if [ "$?" -gt "0" ];
+  then
+    echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
+    echo "apt-get update"
+    echo "apt -y install software-properties-common"
+    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
+    echo "apt-get update"
+    echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
+libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git pwgen curl libdb4.8-dev \
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev "
+ exit 1
+fi
+}
+
+function create_swap() {
+ echo -e "Checking if swap space is needed."
+ PHYMEM=$(free -g|awk '/^Mem:/{print $2}')
+ SWAP=$(free -g|awk '/^Swap:/{print $2}')
+ if [ "$PHYMEM" -lt "2" ] && [ -n "$SWAP" ]
+  then
+    echo -e "${GREEN}Server is running with less than 2G of RAM without SWAP, creating 2G swap file.${NC}"
+    SWAPFILE=$(mktemp)
+    dd if=/dev/zero of=$SWAPFILE bs=1024 count=2M
+    chmod 600 $SWAPFILE
+    mkswap $SWAPFILE
+    swapon -a $SWAPFILE
+ else
+  echo -e "${GREEN}Server running with at least 2G of RAM, no swap needed.${NC}"
+ fi
+ clear
+}
+
+
+function important_information() {
+ echo
+ echo -e "================================================================================================================================"
+ echo -e "$COIN_NAME Masternode is up and running listening on port ${RED}$COIN_PORT${NC}."
+ echo -e "Configuration file is: ${RED}$CONFIGFOLDER/$CONFIG_FILE${NC}"
+ echo -e "Start: ${RED}/etc/init.d/$COIN_NAME start${NC}"
+ echo -e "Stop: ${RED}/etc/init.d/$COIN_NAME stop${NC}"
+ echo -e "VPS_IP:PORT ${RED}$NODEIP:$COIN_PORT${NC}"
+ echo -e "MASTERNODE PRIVATEKEY is: ${RED}$COINKEY${NC}"
+ echo -e "Check if $COIN_NAME is running by using the following command: ${RED}ps -ef | grep $COIN_NAME | grep -v grep${NC}"
+ echo -e "================================================================================================================================"
+}
+
+function setup_node() {
+  get_ip
+  create_config
+  create_key
+  update_config
+  enable_firewall
+  important_information
+  configure_startup
+}
+
+
+##### Main #####
+clear
+
+checks
+#prepare_system
+#create_swap
+#delete_wallet
+#compile_node
+if (( $INSTALLED == 0 )); then
+ setup_node
+else
+ /etc/init.d/$COIN_NAME start
+ echo -e "${RED}New wallet compiled and installed. Old configuration was kept intact.${NC}"
+ echo -e "Old configuration folder backed up as ${RED}$CONFIGFOLDER.bkp${NC}"
+fi
