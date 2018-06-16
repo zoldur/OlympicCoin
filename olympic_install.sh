ccfe288a5fce2abddb30a23397a6913bdf00c746
Zoldur <zoldur99@gmail.com>
2018-06-16 03:19:27 -0700

    Get IP

--- olympic_install.sh
+++ olympic_install.sh
@@ -10,7 +10,7 @@ COIN_NAME='Olympic'
 COIN_PORT=26667
 RPC_PORT=26666
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -128,7 +128,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
